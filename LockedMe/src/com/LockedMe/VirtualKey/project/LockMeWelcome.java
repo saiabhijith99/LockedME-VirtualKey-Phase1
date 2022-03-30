7208abf70850dad1b837435cc975751f25c67939
Hp <Hp@DESKTOP-70571AL>
2022-03-30 15:05:04 +0530

    Project Pushing Phase-1

---
+++ LockedMe/src/com/LockedMe/VirtualKey/project/LockMeWelcome.java
@@ -0,0 +1,17 @@
+package com.LockedMe.VirtualKey.project;
+
+public class LockMeWelcome {
+	public  void LockMeWelcome()
+	{
+	String str="JYOTHI SAI ABHIJITH TALLAM";
+	System.out.println();
+	System.out.println("                                  *****   Welcome To LockMe.com Application   *****               ");
+	System.out.println("                                              Create Your Repository	");
+	System.out.println();
+	 System.out.println("                                   ===========================================================");
+	System.out.println("                                             The Developer is  @"+str );
+	System.out.println("                                    ============================================================");
+	}
+
+
+}
